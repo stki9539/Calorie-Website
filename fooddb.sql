b9db27aa40c9478fde496d63de50aed467508ab2
Sibo Song <siso8575@colorado.edu>
2016-03-07 14:40:03 -0700

    Update calories for 'protein' table

--- fooddb.sql
+++ fooddb.sql
@@ -1,5 +1,5 @@
 
-create table if not exists `meat` (
+create table if not exists `protein` (
   `id` int(1) not null auto_increment,
   `name` varchar(40) not null,
   `qty` int(1) not null,
@@ -8,12 +8,12 @@ create table if not exists `meat` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=7;
 
 insert into `protein` (`id`, `name`, `100g`, `calories`) values          'grains fruits vegetables protein diary fats oils sweets'
-  (1, 'chicken', 1, 0),
-  (2, 'beef', 1, 0),
-  (3, 'lamb', 1, 0),
-  (6, 'tuna', 1, 0),
-  (5, 'salmon', 1, 0),
-  (6, 'pork', 1, , 0),
-  (7, 'buffalo',1 , 0),
-  (8, 'turkey' ,1 , 0);
+  (1, 'chicken', 1, 239),
+  (2, 'beef', 1, 213),
+  (3, 'lamb', 1, 294),
+  (6, 'tuna', 1, 184),
+  (5, 'salmon', 1, 208),
+  (6, 'pork', 1, 242),
+  (7, 'buffalo', 1, 99),
+  (8, 'turkey' , 1, 135);
 
