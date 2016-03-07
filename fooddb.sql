b9e700d5a15bc3faf8c3bf96836452348394c960
Sibo Song <siso8575@colorado.edu>
2016-03-07 14:41:50 -0700

    Update fooddb.sql

--- fooddb.sql
+++ fooddb.sql
@@ -2,8 +2,8 @@
 create table if not exists `protein` (
   `id` int(1) not null auto_increment,
   `name` varchar(40) not null,
-  `qty` int(1) not null,
-  `price` float not null,
+  `100g` int(1) not null,
+  `calories` float not null,
   primary key (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=7;
 
