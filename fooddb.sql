7d6c6273b184dd05b2cbdcebf823e81753ff41c2
nba5137 <siso8575@colorado.edu>
2016-03-07 14:27:51 -0700

    Create a food database

---
+++ fooddb.sql
@@ -0,0 +1,19 @@
+
+create table if not exists `meat` (
+  `id` int(1) not null auto_increment,
+  `name` varchar(40) not null,
+  `qty` int(1) not null,
+  `price` float not null,
+  primary key (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=7;
+
+insert into `protein` (`id`, `name`, `100g`, `calories`) values          'grains fruits vegetables protein diary fats oils sweets'
+  (1, 'chicken', 1, 0),
+  (2, 'beef', 1, 0),
+  (3, 'lamb', 1, 0),
+  (6, 'tuna', 1, 0),
+  (5, 'salmon', 1, 0),
+  (6, 'pork', 1, , 0),
+  (7, 'buffalo',1 , 0),
+  (8, 'turkey' ,1 , 0);
+
