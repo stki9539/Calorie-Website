fd7846e93c5cc2f8af0de48517fe58d1bb948220
tiwa9436 <tiwa9436@colorado.edu>
2016-04-25 02:03:39 -0600

    Added logout.php

---
+++ logout.php
@@ -0,0 +1,36 @@
+
+<?php
+
+    session_start();
+
+    session_destroy();
+
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <title>You are logged out!</title>
+
+    </head>
+
+    <body>
+
+        You are logged out.<br />
+
+    
+
+        <a href="Login.php">Login</a>
+
+    </body>
+
+</html>
